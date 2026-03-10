5c780e3afe3bb64ab7337b3b8e6f7b2cd756406b
Samuel-ketema <ketemasamuelmez@gmail.com>
2026-03-10 17:13:25 +0300

    Add summarization functionality using bedrock_client

--- Src/summarize.py
+++ Src/summarize.py
@@ -1,1 +1,13 @@
+from bedrock_client import invoke_model
+from prompts import summarize_prompt
 
+text = """
+Artificial intelligence is transforming industries by enabling automation,
+predictive analytics, and intelligent decision making.
+"""
+
+prompt = summarize_prompt(text)
+
+response = invoke_model(prompt)
+
+print(response)
