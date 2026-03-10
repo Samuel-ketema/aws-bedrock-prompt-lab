6a4d548546c392f88ad1317c8b1148fc481eeeee
Samuel-ketema <ketemasamuelmez@gmail.com>
2026-03-10 17:14:17 +0300

    Add JSON extraction and model invocation functionality

--- Src/extract_json.py
+++ Src/extract_json.py
@@ -1,1 +1,13 @@
+from bedrock_client import invoke_model
+from prompts import extract_prompt
 
+text = """
+John Smith works at Amazon.
+His email is johnsmith@email.com
+"""
+
+prompt = extract_prompt(text)
+
+response = invoke_model(prompt)
+
+print(response)
