85d61cc2648c07a528a1ad0030790f95080a3ae7
Samuel-ketema <ketemasamuelmez@gmail.com>
2026-03-10 17:13:52 +0300

    Add classification functionality using bedrock_client

--- Src/classify.py
+++ Src/classify.py
@@ -1,1 +1,10 @@
+from bedrock_client import invoke_model
+from prompts import classify_prompt
 
+text = "The product works great but customer support was slow."
+
+prompt = classify_prompt(text)
+
+response = invoke_model(prompt)
+
+print(response)
